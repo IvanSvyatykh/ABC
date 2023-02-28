0aaeb8231faec7ebda10316850e4cae2c5804b48
Ivan Svyatykh <Zlativansvyat@mail.ru>
2023-02-28 23:46:19 +0500

    Простейщие операции на асамблере

--- ABC/ABC.cpp
+++ ABC/ABC.cpp
@@ -1,50 +1,23 @@
 ﻿#include <iostream>
 using namespace std;
-void ShowByte(unsigned char* data )
-{
-	for (int i = 0; i < 8; i++)
-	{
-		if ((*data) & (0x80 >> i)) printf("1");
-		else  printf("0");
-	}
-	cout << " ";
-}
 
-void ShowFull(unsigned char* number, int NumOfBytes)
-{
-
-	cout << "Big Endian :";
-	for (int i = NumOfBytes - 1; i >= 0; i--)
-	{
-		ShowByte((number)+i);
-
-	}
-	cout << endl;
-	cout << "Little Endian :";
-	for (int i = 0; i < NumOfBytes; i++)
-	{
-		ShowByte((number)+i);
-
-	}
-}
+int x;
 
 int main()
 {
-    int num;
-    cin >> num;
-
-    int numInt = num;
-    float numFloat = num;
-    double numDouble = num;
-		
-	cout << "int" << endl;
-	ShowFull((unsigned char*)&numInt , 4);
-	cout << endl;
-	
-	cout << "float" << endl;
-	ShowFull((unsigned char*)&numFloat, 4);
-	cout << endl;
-
-	cout << "double" << endl;
-	ShowFull((unsigned char*)&numDouble, 8);
+    int a=2, b=2, c=3, d=2, e=3, x;
+    __asm
+    {
+        mov eax,a
+        add eax,b
+        mul c
+        sub eax,d
+        cdq
+        div e
+        mov x ,eax
+        
+    }
+    
+    cout << x;
+    return 0;
 }
