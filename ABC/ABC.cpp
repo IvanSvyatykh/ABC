34a3ece727aba04c3289b21f8efd9852475c9a95
Ivan Svyatykh <Zlativansvyat@mail.ru>
2023-03-02 18:24:16 +0500

    Лаба номер 3

--- ABC/ABC.cpp
+++ ABC/ABC.cpp
@@ -1,23 +1,43 @@
 ﻿#include <iostream>
 using namespace std;
 
-int x;
+
 
 int main()
 {
-    int a=2, b=2, c=3, d=2, e=3, x;
-    __asm
-    {
-        mov eax,a
-        add eax,b
-        mul c
-        sub eax,d
-        cdq
-        div e
-        mov x ,eax
-        
-    }
-    
-    cout << x;
-    return 0;
+	unsigned char numberOne, numberTwo, y;
+	int one, two;
+	char x;
+	bool _signed, _unsigned;
+	cout << "Add Two number" << endl;
+	cin >> one;
+	cout << endl;
+	cin >> two;
+	numberOne = one;
+	numberTwo = two;
+	_asm
+	{
+		mov al, numberOne
+		mov bl, numberTwo
+		mov _signed, 0
+		mov _unsigned, 0
+		add al, bl
+		jno sign_norm;
+		mov _signed, 1
+			sign_norm:
+		jnc unsign_norm;
+		mov _unsigned, 1
+			unsign_norm:
+		mov x, al
+			mov y, al
+	}
+	cout << "numberOne + numberTwo = " << (int)y << "(unsigned)" << endl;
+	cout << "numberOne + numberTwo = " << (int)x << "(signed)" << endl;
+
+
+	if (_signed) cout << "overflow (signed)" << endl;
+	else cout << "no overflow (signed)" << endl;
+	if (_unsigned) cout << "overflow (unsigned)" << endl;
+	else cout << "no overflow (unsigned)" << endl;
+
 }
